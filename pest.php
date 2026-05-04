f63b5d0c131bb8162546251111b44ecdcba0cd93
Miguelex <miguelexd@gmail.com>
2026-05-04 18:23:16 +0200

    fix: resolve Pest/PHPUnit conflict and add lockfiles

--- pest.php
+++ pest.php
@@ -12,7 +12,7 @@ declare(strict_types=1);
 |
 */
 
-uses(Tests\TestCase::class)->in('Feature', 'Integration');
+uses(Tests\TestCase::class)->in('Feature');
 
 /*
 |--------------------------------------------------------------------------
